89634c7248069f6868b3fb6949d3463f1bb9fc9e
blood8879 <blood8879@gmail.com>
2023-03-05 22:52:50 +0900

    20230305

---
+++ client/pages/team/[id]/[fixture]/[fixtureid].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const fixtureDetail: NextPage = () => {
+    return (
+        <div>
+            일정상세
+        </div>
+    )
+}
+
+export default fixtureDetail;
