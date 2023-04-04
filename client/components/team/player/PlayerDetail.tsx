2a7b76b8e99c7442a6a765ff0528c330b48e10f7
blood8879 <blood8879@gmail.com>
2023-04-04 22:29:11 +0900

    20230404

--- client/components/team/player/PlayerDetail.tsx
+++ client/components/team/player/PlayerDetail.tsx
@@ -8,19 +8,21 @@ const PlayerDetail: React.FC = () => {
     const team = useSelector((state) => state.team.detail);
     const user = useSelector((state) => state.user);
     const playerstat = useSelector((state) => state.stat.detail);
+    const squad = useSelector((state) => state.squad.squad);
     const { userId } = router.query;
     
     return (
         <div>
             {playerstat.length > 0 ? (
                 <>
+                    {squad.find((p) => p.userId._id === userId)?.backNo}  {squad.find((p) => p.userId._id === userId)?.userId.name}
                     <h2>totalCaps: {playerstat[0]?.totalCaps || 0}</h2>
                     <h2>totalGoals: {playerstat[0]?.totalGoals || 0}</h2>
                     <h2>totalAssists: {playerstat[0]?.totalAssists || 0}</h2>    
                 </>
             ) : (
                 <>
-                    <h2>{userId}</h2>
+                    {squad.find((p) => p.userId._id === userId)?.backNo}  {squad.find((p) => p.userId._id === userId)?.userId.name}
                     <h2>totalCaps: 0</h2>
                     <h2>totalGoals: 0</h2>
                     <h2>totalAssists: 0</h2>
--- client/pages/team/[id]/teamManage.tsx
+++ client/pages/team/[id]/teamManage.tsx
@@ -1,15 +1,22 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import TeamDetail from "../../../components/team/TeamDetail";
+import { useSelector } from "../../../store";
 
 const Manage: NextPage = () => {
+    const team = useSelector((state) => state.team);
+
     return (
         <TeamDetail>
             <div>
                 <div className="space-x-3">
-                    <Link href="/team/registerFixture">
-                        경기등록
-                    </Link>
+                    {team.stadium.length > 0 ? (
+                        <Link href="/team/registerFixture">
+                            경기등록
+                        </Link>
+                    ): (
+                        <></>
+                    )}
                     <Link href="/team/manage/squadManage">
                         팀원관리
                     </Link>
