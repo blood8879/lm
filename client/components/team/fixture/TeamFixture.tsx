813267eacefacc597de1897556d94a0b00f1ab8e
blood8879 <blood8879@gmail.com>
2023-04-23 00:21:50 +0900

    20230423

--- client/components/team/fixture/TeamFixture.tsx
+++ client/components/team/fixture/TeamFixture.tsx
@@ -7,10 +7,45 @@ import 'moment/locale/ko';
 const TeamFixture: React.FC = () => {
     const team = useSelector((state) => state.team.detail?._id);
     const fixtures = useSelector((state) => state.fixture.fixture);
+
+    // 매치데이트별로 그룹핑하는 함수
+    // const groupByDate = (fixtures: any) => {
+    //     const result: any = {};
+    //     fixtures.forEach((fixture: any) => {
+    //     const date = moment(fixture.matchDay).format("YYYY-MM-DD");
+    //     if (!result[date]) {
+    //         result[date] = [];
+    //     }
+    //     result[date].push(fixture);
+    //     });
+    //     return result;
+    // };
+
+    // const groupedFixtures = groupByDate(fixtures);
     
     return (
         <TeamDetail>
-            <div>
+            <div className="h-64 bg-gradient-to-r from-gray-900 to-gray-100">
+                <div className="flex w-full py-24 px-[33.3em] items-center">
+                    <h1 className="text-white text-6xl font-bold">Fixtures</h1>
+                </div>
+            </div>
+            <div className="self-auto px-[33.3em] py-4">
+                {fixtures.map((fixture, key) => (
+                    <ul key={fixture._id} className="space-y-4">
+                        <h2 className="text-xl font-bold mb-2">{moment(fixture.matchDay).add(-9, 'h').format('yyyy-MM-DD')}</h2>
+                        <Link href={`/team/${team}/fixture/${fixture._id}`}>
+                            <li className="h-8 center space-x-2 hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white">
+                                <span className="text-bold-4">{fixture.homeTeam.name}</span> 
+                                <span className="border-2">{moment(fixture.matchDay).add(-9, 'h').format('HH:mm')}</span>
+                                <span className="text-bold">{fixture.awayTeam.name}</span>
+                                <span>{fixture.venue}</span>
+                            </li>
+                        </Link>
+                    </ul>
+                ))}
+            </div>
+            {/* <div className="flex justify-center items-center">
                 {fixtures.map((fixture, key) => (
                     <ul key={fixture._id}>
                         <h2 className="text-xl bold">{moment(fixture.matchDay).add(-9, 'h').format('yyyy-MM-DD')}</h2>
@@ -22,18 +57,9 @@ const TeamFixture: React.FC = () => {
                                 <span>{fixture.venue}</span>
                             </li>
                         </Link>
-                        {/* <span>{fixture.venue}</span> */}
                     </ul>
-                    // <div key={fixture._id}>
-                    //     <Link href={`/team/${team}/fixture/${fixture._id}`}>
-                    //         <h2 className="w-4 center">{fixture.homeTeam.name}</h2>
-                    //         <p>vs</p>
-                    //         <h2 className="w-4 center text-blue-500">{fixture.awayTeam.name}</h2>
-                    //     </Link>
-                    //     {/* <img src={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/emblem/${team.emblem}`} alt="emblem" /> */}
-                    // </div>
                 ))}
-            </div>
+            </div> */}
         </TeamDetail>
     );
 };
