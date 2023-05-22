a3d865ec625c4d3ba06a08d08cdfe1e1bf0df48f
blood8879 <blood8879@gmail.com>
2023-05-22 22:09:37 +0900

    20230522

--- client/components/team/fixture/ResultDetail.tsx
+++ client/components/team/fixture/ResultDetail.tsx
@@ -1,5 +1,6 @@
 import moment from "moment";
 import Head from "next/head";
+import Image from "next/image";
 import { useSelector } from "../../../store";
 
 const ResultDetail: React.FC = () => {
@@ -16,16 +17,10 @@ const ResultDetail: React.FC = () => {
                 <h4 className="text-xs bold">{moment(matchInfo.matchDay).add(-9, 'h').format('yyyy-MM-DD')}</h4>
                 <h4 className="text-xs bold">{matchInfo.venue}</h4>
             </div>
-            <div className="flex container mx-auto mt-4 h-8">
+            <div className="flex container mx-auto mt-4 h-12">
                 <div className="w-[48%]">
-                    <span className="flex justify-end bg-[#37003c] text-white h-8 py-1">{matchInfo.homeTeam.name}&nbsp;&nbsp;</span>
-                </div>
-                <div className="w-[4%] space-x-4 grid place-items-center bg-pink-500 text-white">{matchInfo.home_goals}-{matchInfo.away_goals}</div>
-                <div className="w-[48%]">
-                    <span className="flex justify-start bg-[#37003c] text-white h-8 py-1">{matchInfo.awayTeam.name}</span>
-                </div>
-            </div>
-            <div>
+                    <span className="flex justify-end bg-[#37003c] text-white h-12"><span className="mr-1 grid place-items-center"><Image className="rounded-full grid place-items-center w-10 h-10" src={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/emblem/${matchInfo.homeTeam.emblem}`} width={50} height={50} alt="img" /></span><span className="pt-2.5 mr-4">{matchInfo.homeTeam.name}</span></span>
+                    <div className="flex container mx-auto mt-4">
                 <h2>득점(홈)</h2>
                 {Object.entries(matchInfo.homePlayerGoals).map(([playerId, goals]) => {
                     const matchingPlayer = squad.find((p: any) => p.userId._id === playerId);
@@ -42,7 +37,47 @@ const ResultDetail: React.FC = () => {
                     }
                 })}
             </div>
-            <div>
+                </div>
+                <div className="w-[4%] space-x-4 grid place-items-center bg-pink-500 text-white">{matchInfo.home_goals}-{matchInfo.away_goals}</div>
+                <div className="w-[48%]">
+                    <span className="flex justify-start bg-[#37003c] text-white h-12"><span className="pt-2.5 ml-4">{matchInfo.awayTeam.name}</span><span className="ml-2 grid place-items-center"><Image className="rounded-full grid place-items-center w-10 h-10" src={`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/emblem/${matchInfo.awayTeam.emblem}`} width={50} height={50} alt="img" /></span></span>
+                    <div className="flex container mx-auto mt-4">
+                        <h2>득점(어웨이)</h2>
+                        {Object.entries(matchInfo.awayPlayerGoals).map(([playerId, goals]) => {
+                            const matchingPlayer = squad.find((p: any) => p.userId._id === playerId);
+                            if(matchingPlayer) {
+                                return (
+                                    <div className="flex space-x-2">
+                                        <h2>{matchingPlayer.backNo}</h2>
+                                        <h2>{matchingPlayer.userId.name}</h2>
+                                        <div>
+                                            <span>{Number(goals)}</span>골
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })}
+                    </div>
+                </div>
+            </div>
+            {/* <div className="flex container mx-auto mt-4">
+                <h2>득점(홈)</h2>
+                {Object.entries(matchInfo.homePlayerGoals).map(([playerId, goals]) => {
+                    const matchingPlayer = squad.find((p: any) => p.userId._id === playerId);
+                    if(matchingPlayer) {
+                        return (
+                            <div className="flex space-x-2">
+                                <h2>{matchingPlayer.backNo}</h2>
+                                <h2>{matchingPlayer.userId.name}</h2>
+                                <div>
+                                    <span>{Number(goals)}</span>골
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
+            </div> */}
+            {/* <div className="flex container mx-auto">
                 <h2>득점(어웨이)</h2>
                 {Object.entries(matchInfo.awayPlayerGoals).map(([playerId, goals]) => {
                     const matchingPlayer = squad.find((p: any) => p.userId._id === playerId);
@@ -58,7 +93,7 @@ const ResultDetail: React.FC = () => {
                         )
                     }
                 })}
-            </div>
+            </div> */}
         </>
     )
 }
